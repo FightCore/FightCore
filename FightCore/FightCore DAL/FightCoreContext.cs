dd01ec942879990e4f1ee3acaab6d238cd983901
Bart de Bever <bartdebever112@gmail.com>
2018-08-12 23:51:09 +0200

    Project setup

---
+++ FightCore/FightCore DAL/FightCoreContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FightCore_Models.Characters;
+using Microsoft.EntityFrameworkCore;
+
+namespace FightCore_DAL
+{
+    public class FightCoreContext : DbContext
+    {
+        public DbSet<Character> Characters { get; set; }
+
+        public FightCoreContext()
+        {             
+        }
+
+        public FightCoreContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=FightCore;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
---
+++ FightCore/FightCore DAL/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query.ExpressionVisitors.Internal;
+
+namespace FightCore_DAL
+{
+    public interface IUnitOfWork
+    {
+        GenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+        GenericRepositoryAsync<TEntity> GetRepositoryAsync<TEntity>() where TEntity : class;
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
