07fefc1c423da9b67a3d7ed6d130aaa8f7730f9e
Bart de Bever <bartdebever112@gmail.com>
2018-08-14 19:10:13 +0200

    Added controllers based on the discussion in #dev_life

---
+++ FightCore/FightCore/Controllers/CharacterController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FightCore.Controllers
+{
+    public class CharacterController : BaseController
+    {
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IActionResult> Character(int characterId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FightCore/FightCore/Controllers/NotificationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FightCore.Controllers
+{
+    public class NotificationController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
