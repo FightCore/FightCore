e1cde85f0ac9b534564443a0ec6872379e79d630
Jawad <jawadmn@gmail.com>
2018-08-17 00:20:12 -0500

    Added sass ability and examples to site

--- FightCore/FightCore/gulpfile.js
+++ FightCore/FightCore/gulpfile.js
@@ -4,11 +4,24 @@ This file is the main entry point for defining Gulp tasks and using Gulp plugins
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
+'use strict';
+
 var gulp = require('gulp');
 var del = require('del');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('Styles/main.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./Styles/**/*.scss', ['sass']);
+});
 
 var paths = {
-  scripts: ['scripts/**/*.js', 'scripts/**/*.ts', 'scripts/**/*.map'],
+  scripts: ['Scripts/**/*.js', 'Scripts/**/*.ts', 'Scripts/**/*.map'],
 };
 
 gulp.task('clean', function () {
