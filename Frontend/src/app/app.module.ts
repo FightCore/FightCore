fa801d09a4218a38add97e1f2d4f9813b575c4e2
martijn broekman <martijn.oisterwijk@outlook.com>
2018-08-29 22:35:18 +0200

    Added the first components

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
