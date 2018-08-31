e661b7ab225fa1e14f9ec3948bcf326b92567c16
Bart de Bever <bartdebever112@gmail.com>
2018-08-31 19:57:48 +0200

    Created new endpints

---
+++ Backend/3 Repositories/FightCore.Repositories/Games/GameRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FightCore.Models;
+using FightCore.Repositories.Patterns;
+using Microsoft.EntityFrameworkCore;
+
+namespace FightCore.Repositories.Games
+{
+    public interface IGameRepository : IRepositoryAsync<Game>
+    {
+        Task<Game> GetGameByIdAsync(int id);
+        Task<List<Game>> GetAllGamesAsync(); 
+    }
+    public class GameRepository: Repository<Game>, IGameRepository
+    {
+        public GameRepository(DbContext context) : base(context)
+        {
+        }
+
+        public Task<Game> GetGameByIdAsync(int id)
+        {
+            return Queryable.Include(x => x.Media).FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public Task<List<Game>> GetAllGamesAsync()
+        {
+            return Queryable.Include(x => x.Media).ToListAsync();
+        }
+    }
+}
--- Backend/6 Api/FightCore.Api/Controllers/TechniquesController.cs
+++ Backend/6 Api/FightCore.Api/Controllers/TechniquesController.cs
@@ -16,13 +16,20 @@ namespace FightCore.Api.Controllers
     {
         private readonly ITechniqueService _techniqueService;
         private readonly IMapper _mapper;
-
+        /// <summary>
+        /// Testing comments
+        /// </summary>
+        /// <param name="techniqueService"></param>
+        /// <param name="mapper"></param>
         public TechniquesController(ITechniqueService techniqueService, IMapper mapper)
         {
             _techniqueService = techniqueService;
             _mapper = mapper;
         }
-
+        /// <summary>
+        /// Get all techniques known to FightCore
+        /// </summary>
+        /// <returns>An array of technique objects</returns>
         [HttpGet("")]
         public async Task<IActionResult> GetAllTechniquesAsync()
         {
@@ -35,7 +42,11 @@ namespace FightCore.Api.Controllers
 
             return Ok(resources);
         }
-
+        /// <summary>
+        /// Get a specific Technique by id
+        /// </summary>
+        /// <param name="id">The Id of the technique object you want.</param>
+        /// <returns>A single technique object</returns>
         [HttpGet("id/{id}")]
         public async Task<IActionResult> GetTechniqueByIdAsync(int id)
         {
@@ -48,7 +59,11 @@ namespace FightCore.Api.Controllers
 
             return Ok(resource);
         }
-
+        /// <summary>
+        /// Get all technique objects for a game.
+        /// </summary>
+        /// <param name="gameId">The id of the game you want the techniques from.</param>
+        /// <returns>An array of technique objects.</returns>
         [HttpGet("game/{gameId}")]
         public async Task<IActionResult> GetTechniquesByGameAsync(int gameId)
         {
