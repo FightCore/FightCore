4e6b3ee3f8141c4d3a369027c893e9a4192eafdf
Jawad <jawadmn@gmail.com>
2018-09-13 22:30:35 -0500

    Remove nameKey from interface

--- Frontend/src/app/games/game-info.interface.ts
+++ Frontend/src/app/games/game-info.interface.ts
@@ -1,6 +1,5 @@
 export interface GameInfo {
     id: number,         // Id representing game on backend as well
     fullName: string,   // Full name of game
-    shortName: string,  // Short name of game
-    nameKey: string,    // Used for retrieving and storing files specifically for this game
+    shortName: string  // Short name of game
 }
