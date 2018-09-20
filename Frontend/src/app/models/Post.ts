d33c7f061f95233fe970c6e8e93ef689e2164ed5
Jawad <jawadmn@gmail.com>
2018-09-19 21:29:57 -0500

    Simple functional posting now done- Can actually create (temporary) posts and view them in library

--- Frontend/src/app/models/Post.ts
+++ Frontend/src/app/models/Post.ts
@@ -1,6 +1,23 @@
 export interface Post {
     id: number;
+
+    // Meta fields
+    categoryId: number;
+    characterIds?: number[];        // Required for all categories except Game-Independent
+    // Combo specific fields
+    targetCharacterIds?: number[];
+    comboTypeIds?: number[];
+    targetPercent?: number;         // TODO: Should be a range
+    comboDmg?: number;
+    comboStarterId?: number;
+
+    // Additional fields
+    skillEstimateId: number;
+    patchId: number;
+    tags: string[];
+
+    // Content fields
     title: string;
-    textContent?: string;
     videoUrl?: string;
+    textContent?: string;
 }
--- Frontend/src/app/services/post.service.ts
+++ Frontend/src/app/services/post.service.ts
@@ -10,16 +10,63 @@ export class PostService {
   constructor() {
     // Initialize mockup posts
     this.posts = [
-      {
+      { // Combo post mockup
+
         id: 1,
+        // Meta fields
+        categoryId: 2,
+        characterIds: [2], 
+
+        // Combo specific fields
+        targetCharacterIds: [2],
+        comboTypeIds: [1,2,3,4,5,6,7],
+        targetPercent: 42,
+        comboDmg: 10,
+        comboStarterId: 2,
+
+        // Additional fields
+        skillEstimateId: 1,
+        patchId: 1,
+        tags: ["First", "Test", "ACombo"],
+
+        // Content fields
         title: "First Test Post",
+        videoUrl: "",
         textContent: "Test content here yay"
       },
-      {
+      { // General post mockup
+
         id: 2,
+        // Meta fields
+        categoryId: 1,
+        characterIds: [3],
+
+        // Additional fields
+        skillEstimateId: 1,
+        patchId: 0,
+        tags: ["Second", "Test", "Something"],
+
+        // Content fields        
         title: "Second Test Post",
+        videoUrl: "",
         textContent: "More test content to write out, gosh dang it"
-      }
+      },
+      { // Game-independent with video
+
+        id: 2,
+        // Meta fields
+        categoryId: 1,
+        
+        // Additional fields
+        skillEstimateId: 1,
+        patchId: 0,
+        tags: ["Third", "Test", "Important"],
+
+        // Content fields        
+        title: "Third Test Post, Much Importante",
+        videoUrl: "https://youtu.be/dQw4w9WgXcQ",
+        textContent: "Very important post, mucho importante"
+      },
     ];
    }
 
