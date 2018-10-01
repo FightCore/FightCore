a33f5f0de109217cd03c1b28f8df93c78443e295
Bart de Bever <bartdebever112@gmail.com>
2018-10-01 22:13:42 +0200

    Added base repository and service

---
+++ Backend/3 Repositories/FightCore.Repositories/Resources/UserResourceRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FightCore.Models.Resources;
+using FightCore.Repositories.Patterns;
+using Microsoft.EntityFrameworkCore;
+
+namespace FightCore.Repositories.Resources
+{
+    public interface IUserResourceRepository : IRepositoryAsync<UserResource>
+    {
+
+    }
+    public class UserResourceRepository : Repository<UserResource>, IUserResourceRepository
+    {
+        public UserResourceRepository(DbContext context) : base(context)
+        {
+        }
+
+    }
+}
