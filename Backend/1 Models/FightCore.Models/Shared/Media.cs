5eeffd8b5bf6563325f1f251cb8247cbd952857e
Bart de Bever <bartdebever112@gmail.com>
2018-10-01 21:16:54 +0200

    Added comments to media as requested in PR

--- Backend/1 Models/FightCore.Models/Shared/Media.cs
+++ Backend/1 Models/FightCore.Models/Shared/Media.cs
@@ -8,9 +8,18 @@ namespace FightCore.Models.Shared
     public class Media : IEntity
     {
         public int Id { get; set; }
+        /// <summary>
+        /// The Url of the actual media item being linked to
+        /// </summary>
         public string Url { get; set; }
         public string Description { get; set; }
+        /// <summary>
+        /// The Url of the source's website
+        /// </summary>
         public string SourceUrl { get; set; }
+        /// <summary>
+        /// The name of the source this media item is taken from
+        /// </summary>
         public string Source { get; set; }
         public MediaType MediaType { get; set; }
     }
