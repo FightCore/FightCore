a0017458c6fc4445ec65fec82f0cc6ff48ea126d
Jawad <jawadmn@gmail.com>
2018-10-14 13:44:43 -0500

    Backend: Add Notifications to database and add second default user

---
+++ Backend/1 Models/FightCore.Models/Notification.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FightCore.Models
+{
+    public class Notification : IEntity
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// The user this notification is for
+        /// </summary>
+        public ApplicationUser User { get; set; }
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// When this notification was created
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+        /// <summary>
+        /// When this notification was actually read by end user
+        /// </summary>
+        public DateTime? ReadDate { get; set; }
+
+        /// <summary>
+        /// Title of the notification
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// The content of the notification
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// On-site relative link for where this notification should lead to
+        /// Security: Be careful with this field, otherwise we may expose security vulnerabilities
+        /// Should not be directly set by a request
+        /// </summary>
+        public string RouteLink { get; set; }
+        /// <summary>
+        /// Determines if this notification is important or not
+        /// </summary>
+        public Boolean IsImportant { get; set; }
+    }
+}
---
+++ Backend/2 Data/FightCore.Data/Migrations/20181014183718_AddSecondUserSeed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FightCore.Data.Migrations
+{
+    public partial class AddSecondUserSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { 2, 0, "680f3083-462f-4c8f-ba9a-c09a44145495", "test2@test.com", true, false, null, "TEST2@TEST.COM", "TEST2", "AQAAAAEAACcQAAAAEEF7WgDaqY347VdczNcxXwYb6F7IkpBvK5zRg/PU/t5hYIAgKGZanV5GJEco41ILUQ==", null, false, "WYJC6FNA3WBJEXMXPVVNJTJOB3ZQLL2D", false, "test2" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumns: new[] { "Id", "ConcurrencyStamp" },
+                keyValues: new object[] { 2, "680f3083-462f-4c8f-ba9a-c09a44145495" });
+        }
+    }
+}
