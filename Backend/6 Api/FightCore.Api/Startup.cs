f2a7f9824d1baf1ca4cd3b88c7f8c1fb03d470ba
Bart de Bever <bartdebever112@gmail.com>
2018-11-10 11:13:55 +0100

    Updated to latest version

--- Backend/6 Api/FightCore.Api/Startup.cs
+++ Backend/6 Api/FightCore.Api/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Threading.Tasks;
-using AspNet.Security.OpenIdConnect.Primitives;
+﻿using AspNet.Security.OpenIdConnect.Primitives;
 using AutoMapper;
 using FightCore.Api.Configurations;
 using FightCore.Api.Notifications;
@@ -13,12 +9,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using OpenIddict.Abstractions;
 using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Threading.Tasks;
 
 namespace FightCore.Api
 {
@@ -30,139 +31,6 @@ namespace FightCore.Api
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddCors(options =>
-            {
-                options.AddPolicy("AllowSpecificOrigin",
-                    builder =>
-                        builder.WithOrigins("http://localhost:4200")
-                            .AllowAnyMethod()
-                            .AllowAnyHeader()
-                            .AllowCredentials()
-                );
-            });
-
-            services.AddSignalR();
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new Info { Title = $"{nameof(FightCore)} API", Version = "v1" });
-                options.IncludeXmlComments($@"{AppDomain.CurrentDomain.BaseDirectory}{nameof(FightCore)}.Api.xml");
-                options.DescribeAllEnumsAsStrings();
-                //o.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new ApiKeyScheme()
-                //{
-                //	Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-                //	Name = "Authorization",
-                //	In = "header",
-                //	Type = "apiKey"
-                //});
-                options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OAuth2Scheme
-                {
-                    Type = "oauth2",
-                    Flow = "password",
-                    TokenUrl = "/connect/token"
-                });
-                options.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>
-                {
-                    { "Bearer", new string[] { } }
-                });
-            });
-
-            services.AddDbContext<ApplicationDbContext>(options =>
-            {
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
-                options.UseOpenIddict();
-            });
-                
-
-            services.AddIdentity<ApplicationUser, IdentityRole<int>>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-
-            services.Configure<IdentityOptions>(options =>
-            {
-                options.ClaimsIdentity.UserNameClaimType = OpenIdConnectConstants.Claims.Name;
-                options.ClaimsIdentity.UserIdClaimType = OpenIdConnectConstants.Claims.Subject;
-                options.ClaimsIdentity.RoleClaimType = OpenIdConnectConstants.Claims.Role;
-            });
-
-            // Register OpenIddict services
-            services.AddOpenIddict()
-                .AddCore(options =>
-                {
-                    options.UseEntityFrameworkCore()
-                        .UseDbContext<ApplicationDbContext>();
-                })
-                .AddServer(options =>
-                {
-                    options.UseMvc();
-                    options
-                        // These endpoints still need to be implemented
-                        //.EnableAuthorizationEndpoint("/connect/authorize")
-                        //.EnableLogoutEndpoint("/connect/logout")
-                        //.EnableIntrospectionEndpoint("/connect/introspect")
-                        .EnableTokenEndpoint("/connect/token")
-                        .EnableUserinfoEndpoint("/api/userinfo");
-
-
-                    options.RegisterScopes(OpenIdConnectConstants.Scopes.Email,
-                        OpenIdConnectConstants.Scopes.Profile,
-                        OpenIddictConstants.Scopes.Roles);
-
-                    options.EnableRequestCaching();
-
-                    options.AllowPasswordFlow();
-                    options.AllowRefreshTokenFlow();
-
-                    options.AcceptAnonymousClients();
-
-                    options.DisableHttpsRequirement();
-
-                    options.UseJsonWebTokens();
-                    options.AddEphemeralSigningKey();
-                });
-
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-            JwtSecurityTokenHandler.DefaultOutboundClaimTypeMap.Clear();
-
-            services.AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer(options =>
-                {
-                    options.Authority = Configuration.GetSection("Jwt:Authority").Value;
-                    options.Audience = "resource_server";
-                    options.RequireHttpsMetadata = false;
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        NameClaimType = OpenIdConnectConstants.Claims.Subject,
-                        RoleClaimType = OpenIdConnectConstants.Claims.Role
-                    };
-                    options.Events = new JwtBearerEvents
-                    {
-                        OnMessageReceived = context =>
-                        {
-                            if (context.Request.Query.TryGetValue("token", out var token))
-                            {
-                                context.Token = token;
-                            }
-
-                            return Task.CompletedTask;
-                        }
-                    };
-                });
-
-            services.AddAutoMapper(option => option.AddProfile(new AutoMapperConfiguration()));
-            services.AddPatterns();
-            services.AddServicesAndRepositories();
-        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -177,30 +45,172 @@ namespace FightCore.Api
                 app.UseHsts();
             }
 
-            app.UseCors("AllowSpecificOrigin");
-            app.UseAuthentication();
+            app.UseCors("AllowSpecificOrigins");
+
             app.UseHttpsRedirection();
 
             app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "FightCore API V1");
-                c.DocExpansion(DocExpansion.None);
-                c.RoutePrefix = "";
-                c.DisplayRequestDuration();
-            });
+            app.UseSwaggerUI(
+                c =>
+                    {
+                        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FightCore API V1");
+                        c.DocExpansion(DocExpansion.None);
+                        c.RoutePrefix = string.Empty;
+                        c.DisplayRequestDuration();
+                    });
 
-            app.UseSignalR(routes =>
-            {
-                routes.MapHub<NotifyHub>("/notify");
-            });
+            app.UseSignalR(routes => { routes.MapHub<NotifyHub>("/notify"); });
+            app.UseAuthentication();
+            app.UseMvc(
+                routes => { routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}"); });
+        }
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(
+                options =>
+                    {
+                        options.AddPolicy(
+                            "AllowSpecificOrigins",
+                            builder => builder.WithOrigins("https://www.fightcore.org").AllowAnyMethod().AllowAnyHeader()
+                                .AllowCredentials());
+                    });
+
+            // Register OpenIddict services
+            services.AddOpenIddict()
+                .AddCore(options => { options.UseEntityFrameworkCore().UseDbContext<ApplicationDbContext>(); })
+                .AddServer(
+                    options =>
+                    {
+                        options.Services.AddCors(
+                            corsOptions =>
+                            {
+                                corsOptions.AddPolicy(
+                                        "AllowSpecificOrigins",
+                                        builder => builder.WithOrigins("https://www.fightcore.org").AllowAnyMethod()
+                                            .AllowAnyHeader().AllowCredentials());
+                            });
+
+                        options.UseMvc();
+                        services.Configure<MvcOptions>(
+                            mvcOptions =>
+                            {
+                                mvcOptions.Filters.Add(
+                                        new CorsAuthorizationFilterFactory("AllowSpecificOrigins"));
+                            });
+
+                        options
+
+                            // These endpoints still need to be implemented
+                            // .EnableAuthorizationEndpoint("/connect/authorize")
+                            // .EnableLogoutEndpoint("/connect/logout")
+                            // .EnableIntrospectionEndpoint("/connect/introspect")
+                            .EnableTokenEndpoint("/connect/token").EnableUserinfoEndpoint("/api/userinfo");
+
+                        options.AllowPasswordFlow();
+
+                        options.RegisterScopes(
+                            OpenIdConnectConstants.Scopes.Email,
+                            OpenIdConnectConstants.Scopes.Profile,
+                            OpenIddictConstants.Scopes.Roles);
+
+                        options.EnableRequestCaching();
+
+                        options.AllowRefreshTokenFlow();
+
+                        options.AcceptAnonymousClients();
+
+                        options.DisableHttpsRequirement();
+
+                        options.UseJsonWebTokens();
+                        options.AddEphemeralSigningKey();
+                    });
+
+            services.AddSignalR();
+
+            services.AddDbContext<ApplicationDbContext>(
+                options =>
+                    {
+                        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                        options.UseOpenIddict();
+                    });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.Configure<MvcOptions>(
+                options => { options.Filters.Add(new CorsAuthorizationFilterFactory("AllowSpecificOrigins")); });
+
+            services.AddSwaggerGen(
+                options =>
+                    {
+                        options.SwaggerDoc("v1", new Info { Title = $"{nameof(FightCore)} API", Version = "v1" });
+                        options.IncludeXmlComments(
+                            $@"{AppDomain.CurrentDomain.BaseDirectory}{nameof(FightCore)}.Api.xml");
+                        options.DescribeAllEnumsAsStrings();
+
+                        // o.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new ApiKeyScheme()
+                        // {
+                        // 	Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                        // 	Name = "Authorization",
+                        // 	In = "header",
+                        // 	Type = "apiKey"
+                        // });
+                        options.AddSecurityDefinition(
+                            JwtBearerDefaults.AuthenticationScheme,
+                            new OAuth2Scheme { Type = "oauth2", Flow = "password", TokenUrl = "/connect/token" });
+                        options.AddSecurityRequirement(
+                            new Dictionary<string, IEnumerable<string>> { { "Bearer", new string[] { } } });
+                    });
+
+
+            services.AddIdentity<ApplicationUser, IdentityRole<int>>().AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(
+                options =>
+                    {
+                        options.ClaimsIdentity.UserNameClaimType = OpenIdConnectConstants.Claims.Name;
+                        options.ClaimsIdentity.UserIdClaimType = OpenIdConnectConstants.Claims.Subject;
+                        options.ClaimsIdentity.RoleClaimType = OpenIdConnectConstants.Claims.Role;
+                    });
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            JwtSecurityTokenHandler.DefaultOutboundClaimTypeMap.Clear();
+
+            services.AddAuthentication(
+                options =>
+                    {
+                        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    }).AddJwtBearer(
+                options =>
+                    {
+                        options.Authority = Configuration.GetSection("Jwt:Authority").Value;
+                        options.Audience = "resource_server";
+                        options.RequireHttpsMetadata = false;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                                                                {
+                                                                    NameClaimType =
+                                                                        OpenIdConnectConstants.Claims.Subject,
+                                                                    RoleClaimType = OpenIdConnectConstants.Claims.Role
+                                                                };
+                        options.Events = new JwtBearerEvents
+                                             {
+                                                 OnMessageReceived = context =>
+                                                     {
+                                                         if (context.Request.Query.TryGetValue("token", out var token))
+                                                         {
+                                                             context.Token = token;
+                                                         }
+
+                                                         return Task.CompletedTask;
+                                                     }
+                                             };
+                    });
+
+            services.AddAutoMapper(option => option.AddProfile(new AutoMapperConfiguration()));
+            services.AddPatterns();
+            services.AddServicesAndRepositories();
         }
     }
 }
