472c91f43325078ffacff3130fbcca7a2fe29723
Jawad <jawadmn@gmail.com>
2018-11-22 13:57:09 -0600

    Simple dash elements on home page plus small style cleanup

--- Frontend/src/app/dashboard/posts-widget/posts-widget.component.ts
+++ Frontend/src/app/dashboard/posts-widget/posts-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'dash-posts-widget',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts-widget.component.css']
 })
 export class PostsWidgetComponent implements OnInit {
+  @Input('type') type: string; // For mockup only supports 'General' and 'Character'
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  hasValidType(): boolean {
+    return this.type && (this.isTypeGeneral() || this.isTypeCharacter());
+  }
+
+  isTypeGeneral(): boolean {
+    return this.type === 'General';
+  }
+
+  isTypeCharacter(): boolean {
+    return this.type === 'Character';
   }
 
 }
