1e5d15d239490c8447dc6bee2abbc35d368a58d5
Jawad <jawadmn@gmail.com>
2018-11-27 10:16:05 -0600

    Add basic search field to add post popup

--- Frontend/src/app/components/post-edit-add/post-edit-add-contents/post-edit-add-contents.component.ts
+++ Frontend/src/app/components/post-edit-add/post-edit-add-contents/post-edit-add-contents.component.ts
@@ -1,20 +1,43 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, AbstractControl } from '@angular/forms';
+import { Post } from 'src/app/models/Post';
 
 @Component({
   selector: 'post-edit-add-contents',
   templateUrl: './post-edit-add-contents.component.html',
-  styleUrls: ['./post-edit-add-contents.component.css']
+  styleUrls: ['./post-edit-add-contents.component.scss']
 })
 export class PostEditAddContentsComponent implements OnInit {
   @Output('done') done = new EventEmitter<number>(); // For closing self in popup as necessary
   
-  constructor() { }
+  form: FormGroup;
+  isLoading: boolean;
+
+  lastSearch: string; // Stores previous search attempt for user feedback
+  results: Post[]; // Results from previous search attempt
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      searchControl: []
+    });
+   }
 
   ngOnInit() {
+  }
+
+  onSearch() {
+    let searchControl = this.searchInputControl;
+
+    console.log('Search with text', searchControl.value);
+
+    this.lastSearch = searchControl.value;
+    this.isLoading = !this.isLoading;
   }
 
   finished(pos: number) {
     this.done.emit(pos);
   }
+
+  get searchInputControl(): AbstractControl { return this.form.get('searchControl'); }
 
 }
