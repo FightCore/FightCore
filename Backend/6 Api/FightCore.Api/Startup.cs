a0ae269569f6638d7dea13fc59c1f9db6fc22b2f
Bart de Bever <bartdebever112@gmail.com>
2019-01-03 16:43:55 +0100

    Startup rework.

--- Backend/6 Api/FightCore.Api/Startup.cs
+++ Backend/6 Api/FightCore.Api/Startup.cs
@@ -22,67 +22,169 @@ using Swashbuckle.AspNetCore.Swagger;
 
 namespace FightCore.Api
 {
+    /// <summary>
+    /// Startup class that is ran by MVC6 to set up the application.
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">The base configuration.</param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        /// <summary>
+        /// Gets the configuration.
+        /// </summary>
+        private IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">The services for Dependency Injection.</param>
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options =>
+
+            RegisterCors(services);
+
+            RegisterSignalR(services);
+
+            RegisterMvc(services);
+
+            RegisterSwagger(services);
+
+            RegisterContext(services);
+
+            RegisterIdentity(services);
+
+            RegisterAuthentication(services);
+
+            services.AddAutoMapper(option => option.AddProfile(new AutoMapperConfiguration()));
+            services.AddPatterns();
+            services.AddServicesAndRepositories();
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="env">The current environment.</param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
             {
-                options.AddPolicy("AllowSpecificOrigin",
-                    builder =>
-                        builder.WithOrigins("http://localhost:4200")
-                            .AllowAnyMethod()
-                            .AllowAnyHeader()
-                            .AllowCredentials()
-                );
-            });
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
 
-            services.AddSignalR();
+            RegisterAuthentication(app);
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            RegisterCors(app);
+            RegisterSwagger(app);
 
+            RegisterSignalR(app);
+
+            RegisterMvc(app);
+        }
+
+        #region Swagger
+        private void RegisterSwagger(IServiceCollection services)
+        {
             services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new Info { Title = $"{nameof(FightCore)} API", Version = "v1" });
-                options.IncludeXmlComments($@"{AppDomain.CurrentDomain.BaseDirectory}{nameof(FightCore)}.Api.xml");
-                options.DescribeAllEnumsAsStrings();
-                //o.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new ApiKeyScheme()
-                //{
-                //	Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-                //	Name = "Authorization",
-                //	In = "header",
-                //	Type = "apiKey"
-                //});
-                options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OAuth2Scheme
                 {
-                    Type = "oauth2",
-                    Flow = "password",
-                    TokenUrl = "/connect/token"
+                    options.SwaggerDoc("v1", new Info { Title = $"{nameof(FightCore)} API", Version = "v1" });
+                    options.IncludeXmlComments($@"{AppDomain.CurrentDomain.BaseDirectory}{nameof(FightCore)}.Api.xml");
+                    options.DescribeAllEnumsAsStrings();
+
+                    //// o.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new ApiKeyScheme()
+                    //// {
+                    //// 	Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    //// 	Name = "Authorization",
+                    //// 	In = "header",
+                    //// 	Type = "apiKey"
+                    //// });
+
+                    options.AddSecurityDefinition(
+                        JwtBearerDefaults.AuthenticationScheme,
+                        new OAuth2Scheme
+                            {
+                                Type = "oauth2",
+                                                                                                  Flow = "password",
+                                                                                                  TokenUrl = "/connect/token"
+                                                                                              });
+                    options.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>
+                                                       {
+                                                           { "Bearer", new string[] { } }
+                                                       });
                 });
-                options.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>
+        }
+
+        private void RegisterSwagger(IApplicationBuilder application)
+        {
+            application
+                .UseSwagger()
+                .UseSwaggerUI(c =>
+                    {
+                        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FightCore API V1");
+                        c.DocExpansion(DocExpansion.None);
+                        c.RoutePrefix = string.Empty;
+                        c.DisplayRequestDuration();
+                    });
+        }
+        #endregion Swagger
+
+        #region Cors
+        private void RegisterCors(IServiceCollection services)
+        {
+            services.AddCors(options =>
                 {
-                    { "Bearer", new string[] { } }
+                    options.AddPolicy(
+                        "AllowSpecificOrigin",
+                        builder => builder.WithOrigins("http://localhost:4200")
+                                .AllowAnyMethod()
+                                .AllowAnyHeader().AllowCredentials());
                 });
-            });
+        }
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-            {
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
-                options.UseOpenIddict();
-            });
-                
+        private void RegisterCors(IApplicationBuilder application)
+        {
+            application.UseCors("AllowSpecificOrigin");
+        }
+        #endregion Cors
 
+        #region Mvc
+
+        private void RegisterMvc(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        private void RegisterMvc(IApplicationBuilder application)
+        {
+            application.UseMvc(routes =>
+                {
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller=Home}/{action=Index}/{id?}");
+                });
+        }
+
+        #endregion
+
+        #region Identity
+
+        private void RegisterIdentity(IServiceCollection services)
+        {
             services.AddIdentity<ApplicationUser, IdentityRole<int>>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
             services.Configure<IdentityOptions>(options =>
             {
@@ -129,78 +231,81 @@ namespace FightCore.Api
 
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
             JwtSecurityTokenHandler.DefaultOutboundClaimTypeMap.Clear();
-
-            services.AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer(options =>
-                {
-                    options.Authority = Configuration.GetSection("Jwt:Authority").Value;
-                    options.Audience = "resource_server";
-                    options.RequireHttpsMetadata = false;
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        NameClaimType = OpenIdConnectConstants.Claims.Subject,
-                        RoleClaimType = OpenIdConnectConstants.Claims.Role
-                    };
-                    options.Events = new JwtBearerEvents
-                    {
-                        OnMessageReceived = context =>
-                        {
-                            if (context.Request.Query.TryGetValue("token", out var token))
-                            {
-                                context.Token = token;
-                            }
-
-                            return Task.CompletedTask;
-                        }
-                    };
-                });
-
-            services.AddAutoMapper(option => option.AddProfile(new AutoMapperConfiguration()));
-            services.AddPatterns();
-            services.AddServicesAndRepositories();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        #endregion
+
+        #region Authentication
+
+        private void RegisterAuthentication(IServiceCollection services)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+            services.AddAuthentication(options =>
+                    {
+                        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    })
+                .AddJwtBearer(options =>
+                    {
+                        options.Authority = Configuration.GetSection("Jwt:Authority").Value;
+                        options.Audience = "resource_server";
+                        options.RequireHttpsMetadata = false;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                                                                {
+                                                                    NameClaimType = OpenIdConnectConstants.Claims.Subject,
+                                                                    RoleClaimType = OpenIdConnectConstants.Claims.Role
+                                                                };
+                        options.Events = new JwtBearerEvents
+                                             {
+                                                 OnMessageReceived = context =>
+                                                     {
+                                                         if (context.Request.Query.TryGetValue("token", out var token))
+                                                         {
+                                                             context.Token = token;
+                                                         }
 
-            app.UseCors("AllowSpecificOrigin");
-            app.UseAuthentication();
-            app.UseHttpsRedirection();
-
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "FightCore API V1");
-                c.DocExpansion(DocExpansion.None);
-                c.RoutePrefix = "";
-                c.DisplayRequestDuration();
-            });
-
-            app.UseSignalR(routes =>
-            {
-                routes.MapHub<NotifyHub>("/notify");
-            });
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+                                                         return Task.CompletedTask;
+                                                     }
+                                             };
+                    });
         }
+
+        private void RegisterAuthentication(IApplicationBuilder application)
+        {
+            application
+                .UseAuthentication()
+                .UseHttpsRedirection();
+        }
+
+        #endregion
+
+        #region SignalR
+
+        private void RegisterSignalR(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        private void RegisterSignalR(IApplicationBuilder application)
+        {
+            application.UseSignalR(routes =>
+                {
+                    routes.MapHub<NotifyHub>("/notify");
+                });
+        }
+
+        #endregion
+
+        #region Context
+
+        private void RegisterContext(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                    options.UseOpenIddict();
+                });
+        }
+
+        #endregion
     }
 }
