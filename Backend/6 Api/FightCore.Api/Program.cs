5019eded01d645f840f5bf78e662525598d0b863
Bart de Bever <Bart.dbever@hetnic.nl>
2019-01-11 16:46:53 +0100

    Added custom appsettings using args

--- Backend/6 Api/FightCore.Api/Program.cs
+++ Backend/6 Api/FightCore.Api/Program.cs
@@ -10,33 +10,40 @@ namespace FightCore.Api
     public class Program
     {
         /// <summary>
-        /// Gets the configuration based off the appsettings.json files.
-        /// </summary>
-        private static IConfiguration Configuration { get; } = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
-            .AddEnvironmentVariables()
-            .Build();
-
-        /// <summary>
         /// The method that is executed when running the program.
         /// </summary>
         /// <param name="args">The provided arguments to run with.</param>
         public static void Main(string[] args)
         {
+			var config = CreateConfiguration(args);
+
             Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(Configuration)
+                .ReadFrom.Configuration(config)
                 .Enrich.FromLogContext()
                 .CreateLogger();
 
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args, config).Build().Run();
         }
 
-        private static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args, IConfiguration configuration)
         {
-            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().UseConfiguration(Configuration)
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().UseConfiguration(configuration)
                 .UseSerilog();
         }
-    }
+
+		private static IConfiguration CreateConfiguration(string[] args)
+		{
+			var configBuilder = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+			foreach(var argument in args)
+			{
+				configBuilder.AddJsonFile($"appsettings.{argument}.json", true, true);
+			}
+
+			configBuilder.AddEnvironmentVariables();
+			return configBuilder.Build();
+		}
+	}
 }
