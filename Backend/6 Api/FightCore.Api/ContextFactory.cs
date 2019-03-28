201bc7c9b8ae22e765608cea78ac93de36671681
Bart de Bever <bartdebever112@gmail.com>
2019-03-28 11:57:48 +0100

    Added doc to ContextFactory

--- Backend/6 Api/FightCore.Api/ContextFactory.cs
+++ Backend/6 Api/FightCore.Api/ContextFactory.cs
@@ -13,8 +13,10 @@ namespace FightCore.Api
     using Microsoft.EntityFrameworkCore.Design;
     using Microsoft.Extensions.Configuration;
 
+    /// <inheritdoc />
     public class ContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
     {
+        /// <inheritdoc />
         public ApplicationDbContext CreateDbContext(string[] args)
         {
             var configuration = new ConfigurationBuilder()
@@ -22,7 +24,7 @@ namespace FightCore.Api
                 .AddJsonFile("appsettings.json")
                 .Build();
             var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var connectionString = configuration.GetConnectionString(ApplicationPaths.MainSqlConnection);
             builder.UseSqlServer(connectionString);
             return new ApplicationDbContext(builder.Options);
         }
